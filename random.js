c301ab87f4316984ccf7a77ae1361e044332ba9a
Moses Onche <onchemoses38@gmail.com>
2024-07-24 14:10:24 +0100

    Upload random genertor project for hosting

---
+++ random.js
@@ -0,0 +1,29 @@
+let quoteText = document.getElementsByTagName("h1")[0];
+let author = document.getElementsByTagName("h2")[0];
+let next = document.getElementsByTagName("button")[1];
+let previous = document.getElementsByTagName("button")[0];
+
+window.onload = randomQuote();
+
+async function randomQuote() {
+  // Save the previous quote in local Storage
+  sessionStorage.setItem('quote', quoteText.textContent);
+  sessionStorage.setItem('author', author.textContent);
+
+  // Query the API for a new quote
+  let url = "https://api.quotable.io/random";
+  let response = await fetch(url);
+  let quoteContent = await response.json();
+  // Write the new quote to HTML
+  quoteText.innerHTML = quoteContent.content;
+  author.innerHTML = '- ' + quoteContent.author;
+}
+
+function getQuote(){
+  // Write the previous quote to HTML
+  quoteText.innerHTML = sessionStorage.getItem('quote');
+  author.innerHTML = sessionStorage.getItem('author');
+};
+
+next.addEventListener("click", randomQuote);
+previous.addEventListener('click', getQuote);
